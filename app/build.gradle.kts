69eaa799c8fbd5ec0403d8e3ef38d2e1d3371bff
st10321423 <st10321423@vcconnect.edu.za>
2025-10-01 21:13:35 +0200

    i have included unit test dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,16 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
 
+    // Unit test (JVM)
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.12")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("app.cash.turbine:turbine:1.1.0") // Flow testing
+    testImplementation("org.robolectric:robolectric:4.13")
+
+    // Android instrumented tests
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
