1ef1132447bc83be5470aa418dec83102d5302f1
JedialR <128793267+JedialR@users.noreply.github.com>
2025-11-16 21:22:35 +0200

    Added Zulu Support

--- app/src/main/java/com/example/prog7314_universe/UniverseApp.kt
+++ app/src/main/java/com/example/prog7314_universe/UniverseApp.kt
@@ -1,22 +1,11 @@
 package com.example.prog7314_universe
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.LocaleListCompat
-import com.example.prog7314_universe.utils.PrefManager
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import java.util.Locale
+import com.example.prog7314_universe.utils.OfflineSupportManager
 
-class UniverseApp : Application() {
-
+class UniVerseApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        val prefManager = PrefManager(applicationContext)
-        val savedLanguage = runBlocking { prefManager.language.first() }
-        val languageTag = savedLanguage.ifBlank { Locale.getDefault().language }
-        val locales = LocaleListCompat.forLanguageTags(languageTag)
-        AppCompatDelegate.setApplicationLocales(locales)
+        OfflineSupportManager.init(this)
     }
 }
